08dc0222f27dbdafa458962033be9d74aa3d8334
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2022-05-26 12:40:05 -0400

    fixed json

---
+++ front/monkey_see_front/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import axios from 'axios'
+
+function App() {
+
+  //________STATES
+
+//____
+  return (
+  <>
+  <header><h1>Monkey See, Monkey Do.</h1></header>
+  </>
+  );
+}
+
+export default App;
