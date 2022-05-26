2743fbd7e11140f9305e05771dcd8ed289133e12
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-05-26 11:04:24 -0500

    get and post route

--- server.js
+++ server.js
@@ -8,10 +8,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req,res) => {
-    res.json('Hello World')
+app.post('/monkey', (req, res) => {
+    Monkey.create(req.body, (err, createdMonkey) => {
+        res.json(createdMonkey)
+    })
 })
+
+app.get('/monkey', (req,res) => {
+    Monkey.find({}, (err, monkeyHome) => {
+        res.json(monkeyHome)
+    })
+})
+
 
 app.listen(3000, () => {
     console.log('listening...');
+})
+
+mongoose.connect('mongodb://localhost:27017/merncrud')
+mongoose.connection.once('open', () => {
+    console.log('connected to mongod...');
 })
